66be63271c9d607fd16c5e8f391361970f6a3aa6
= <=>
2022-01-25 00:03:15 +0700

    update

--- Danh_sach_lien_ket/linked_list.cpp
+++ Danh_sach_lien_ket/linked_list.cpp
@@ -2,6 +2,7 @@
 node* create_node(int data) {
 	node* new_node = new node;
 	new_node->data = data;
+	new_node->next_ptr = NULL;
 	return new_node;
 }
 void create_list(list& linked_list) {
@@ -11,13 +12,20 @@ void create_list(list& linked_list) {
 void add_node(list&linked_list,node*node) {
 	if (linked_list.head_node == NULL) {
 		linked_list.head_node = node;
+		linked_list.tail_node = node;
 	}
 	node->next_ptr = linked_list.head_node;
 	linked_list.head_node = node;
 }
 void show_list(list linked_list) {
-	for (node *node = linked_list.head_node;node!=linked_list.tail_node; ++node)
-	{
-		std::cout << node->data << "----";
+	if (linked_list.head_node != NULL) {
+		node* new_node = linked_list.head_node;
+		while (new_node != linked_list.tail_node) {
+			std::cout << new_node->data << " ";
+			new_node = new_node->next_ptr;
+
+		}
+
 	}
+
 }
